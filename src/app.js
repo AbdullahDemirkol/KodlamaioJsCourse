b2a24191e3b91553d417e5fbcab2ed8f6fa52c70
Abdullah <abdullahdemirkol01@gmail.com>
2021-10-19 02:21:18 +0300

    Diziler ve for döngüsü kullanıldı.

---
+++ src/app.js
@@ -0,0 +1,27 @@
+console.log("Sa Jekee");
+
+var dolarBugun = 9.3;
+console.log(dolarBugun);
+dolarBugun = 9.5;
+console.log(dolarBugun);
+
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi"];
+
+console.log("<ul>");
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>");
+}
+console.log("</ul>");
+
+{/* <ul>
+    <li>Konut Kredisi</li>
+    <li>Emlak Konut Kredisi</li>
+    <li>Kamu Konut Kredisi</li>
+</ul> */}
+
+console.log(konutKredileri);
+
+let users = getUsersFromApi();
